34ede6320e3d1e77a1d1e25eef2e71313387dcc4
sebaditucci <sebastiandit240@gmail.com~>
2026-04-22 16:18:21 -0300

    rewrite completo

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
         }
     });    
-});
+}, { threshold: 0.15 });
 
 elementos.forEach(element => {
     observer.observe(element);
